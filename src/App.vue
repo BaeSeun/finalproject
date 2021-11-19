a608f4c3e815a956eb0019b56b3317a65ba34dad
김배승 <baebaeseun@naver.com>
2021-11-19 10:37:51 +0900

    sijac

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <navi-bar />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import NaviBar from "./components/layout/NaviBar.vue";
+
+export default {
+  name: "App",
+  components: {
+    NaviBar,
+  },
+};
+</script>
+
+<style>
+html {
+  background-color: #86E57F;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+#app{
+  background-color: #86E57F;
+  background-size: cover;
+}
+a:hover {
+  text-decoration: none;
+  font-weight: bold;
+}
+
+a.router-link-exact-active {
+  color: #24ad1a;
+  font-weight: bold;
+}
+</style>
---
+++ src/components/layout/NaviBar.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="navv">
+    <b-navbar class="nav-custom" toggleable="lg" type="dark">
+      <b-navbar-brand href="#">
+        <router-link to="/">
+          <img
+            src="@/assets/ssafy_logo.png"
+            class="d-inline-block align-middle"
+            width="70px"
+            alt="Kitten"
+          />
+        </router-link>
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#"
+            ><router-link :to="{ name: 'Home' }" class="link"
+              > HOME</router-link
+            ></b-nav-item
+          >
+          <b-nav-item href="#"
+            ><router-link :to="{ name: 'Board' }" class="link"
+              > QNA</router-link
+            ></b-nav-item
+          >
+          <b-nav-item href="#"
+            ><router-link :to="{ name: 'Instargram' }" class="link"
+              >
+              SNS</router-link
+            ></b-nav-item
+          >
+          <b-nav-item href="#"
+            ><router-link :to="{ name: 'House' }" class="link"
+              > APT
+              INFO</router-link
+            ></b-nav-item
+          >
+        </b-navbar-nav>
+
+        <b-navbar-nav class="ml-auto">
+            <b-dropdown-item href="#"
+              ><router-link :to="{ name: 'SignUp' }" class="link"
+                > JOIN</router-link
+              ></b-dropdown-item
+            >
+            <b-dropdown-item href="#"
+              ><router-link :to="{ name: 'SignIn' }" class="link"
+                > LOGIN</router-link
+              ></b-dropdown-item
+            >
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NaviBar",
+};
+</script>
+
+<style>
+
+.nav-custom {
+  background-image: url("wood.jpeg");
+  background-size: cover;
+}
+</style>
