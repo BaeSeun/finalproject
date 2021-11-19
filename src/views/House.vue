69d32ecc2b3e6776d5a7ef223df879674ed91493
rohhy <rohhy@naver.com>
2021-11-19 11:11:23 +0900

    hayoon

--- src/views/House.vue
+++ src/views/House.vue
@@ -1,13 +1,17 @@
 <template>
   <b-container class="bv-example-row mt-3 text-center">
-    <h3 class="underline-orange">
-      <b-icon icon="house-fill"></b-icon> House Service
-    </h3>
+    <img
+              src="@/assets/aptinfoimg1.png"
+              class="d-inline-block align-middle"
+              width="700px"
+              alt="Kitten"
+            />
     <b-row>
       <b-col>
         <house-search-bar></house-search-bar>
       </b-col>
     </b-row>
+    
     <b-row>
       <b-col cols="6" align="left">
         <house-list />
@@ -33,12 +37,5 @@ export default {
 };
 </script>
 <style scoped>
-.underline-orange {
-  display: inline-block;
-  background: linear-gradient(
-    180deg,
-    rgba(255, 255, 255, 0) 70%,
-    rgba(231, 149, 27, 0.3) 30%
-  );
-}
+
 </style>
